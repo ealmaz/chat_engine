e187f33c478e3aa364ff52981c6f42554f1d802f
ealmaz <ealmaz@nurtelecom.kg>
2023-02-13 10:35:46 +0600

    DEV-9049 -> fix typing holder

--- chat_engine/src/main/java/kg/nurtelecom/chat_engine/base/chat/adapter/TypingViewHolder.kt
+++ chat_engine/src/main/java/kg/nurtelecom/chat_engine/base/chat/adapter/TypingViewHolder.kt
@@ -2,18 +2,14 @@ package kg.nurtelecom.chat_engine.base.chat.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import kg.nurtelecom.chat_engine.R
-import kg.nurtelecom.chat_engine.custom_views.message.MessageRoundedCorners
 import kg.nurtelecom.chat_engine.databinding.ItemTypingBinding
-import kg.nurtelecom.chat_engine.model.MessageType
 
 class TypingViewHolder(private val vb: ItemTypingBinding): RecyclerView.ViewHolder(vb.root) {
 
     fun onBind() {
-        vb.container.setupMessageRoundedCorners(MessageType.SYSTEM, MessageRoundedCorners.ALL)
         Glide.with(vb.ivLoader)
             .asGif()
             .load(R.raw.loading)
